7e74347ecf2768e2abf5c52e6a9570ceb68f1054
Ashutosh Tripathy <ashutoshtripathy@gofynd.com>
2025-08-07 23:31:08 +0530

    changes

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
 import uvicorn
+from config.loaded_config import loaded_config
+
 if __name__ == "__main__":
-    uvicorn.run("ai_agents.leadgen.api.main:app", host="0.0.0.0", port=80,workers=1,reload=True)
+    uvicorn.run("ai_agents.leadgen.api.main:app", host="0.0.0.0", port=80,workers=1,reload=loaded_config.debug)
