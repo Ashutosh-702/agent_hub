065d52f46883985997d8e27047e66e90a2efb662
Ahmed <ahmedropewala@fynd.com>
2025-08-07 18:48:48 +0530

    fixes

--- server.py
+++ server.py
@@ -1,3 +1,3 @@
 import uvicorn
 if __name__ == "__main__":
-    uvicorn.run("app.leadgen.main:app", host="0.0.0.0", port=80, reload=True)
+    uvicorn.run("app.ai_agents.leadgen.main:app", host="0.0.0.0", port=80, reload=True)
