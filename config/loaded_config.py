69927d9e44739c531e4d1ab2c38d19fe0aa1ef01
Ashutosh Tripathy <ashutoshtripathy@gofynd.com>
2025-08-07 23:42:41 +0530

    ID:LOGI-2089;DONE:100;HOURS:2

--- config/loaded_config.py
+++ config/loaded_config.py
@@ -5,6 +5,7 @@ from database.connection_manager import ConnectionManager
 class Settings:
     mongo_uri = os.getenv("MONGO_LINKEDIN_SDR_READ_WRITE","mongodb://host.docker.internal:27017")
     connection_manager: ConnectionManager = None
+    debug = os.getenv("DEBUG",False)
 
 
 loaded_config = Settings()
