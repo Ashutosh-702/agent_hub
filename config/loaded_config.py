5bb835db3558e7e6e361c5beced63b0dd15d9608
Ashutosh Tripathy <ashutoshtripathy@gofynd.com>
2025-09-11 00:20:34 +0530

    ID:LOGI-2089;DONE:100;HOURS:2; changes

--- config/loaded_config.py
+++ config/loaded_config.py
@@ -9,6 +9,6 @@ class Settings:
     MODE = os.getenv("MODE", "server")
     base_url = os.getenv("BASE_URL","http://0.0.0.0:80")
     # lusha_api_key = os.getenv("LUSHA_API_KEY")
-    lusha_api_key = "19181599-49af-402c-adb0-71cd402aec81"
+    lusha_api_key = "cc6b0756-527d-4c71-b7e1-fe9119e96cca"
 
 loaded_config = Settings()
--- kafkautils/handlers.py
+++ kafkautils/handlers.py
@@ -321,6 +321,7 @@ async def lusha_company_data_collection(campaign_details: Any):
                     page_payload["total_results"] = total_results
                     page_payload["raw_config"] = campaign_details["raw_config"]
                     scheduler = await schedule_lusha_company_collection(campaign_details=page_payload, eta=eta)
+                    print(f" lusha company data collection scheduler: {scheduler}")
                     break
                 else:
                     break
