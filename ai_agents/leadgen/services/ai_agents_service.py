90ca68e1ead5f4ea730fef3ed55d7d15b23ad80b
Ashutosh Tripathy <ashutoshtripathy@gofynd.com>
2025-10-15 14:56:04 +0530

    changes

--- ai_agents/leadgen/services/ai_agents_service.py
+++ ai_agents/leadgen/services/ai_agents_service.py
@@ -359,13 +359,15 @@ class ContactService:
 
     async def create_contact(self, contact_doc: ContactDocument, campaign_id: str):
         contact_id = await self.contacts_dao.create_contact(contact_doc.dict())
+        raw_data = contact_doc.get("metadata", {}).get("lusha_raw_data", {})
         contact_data =  {
             "campaign_id": campaign_id,
             "company_id": contact_doc.company_id,
             "contact_id": contact_id,
             "metadata": {
                 "created_at": datetime.utcnow(),
-                "updated_at": datetime.utcnow() 
+                "updated_at": datetime.utcnow(),
+                "lusha_raw_data": raw_data
             }
         }
         await self.insert_campaign_contact_run(contact_data)
